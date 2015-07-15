b27f337786868b7d9d13bb8738269c66834083b6
Melissa Leigh Gore <melissaleighgore@gmail.com>
2015-07-15 18:53:12 -0400

    Hound styling revisions

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,4 +1,4 @@
-require 'csv'
+require "csv"
 
 CSV.foreach("db/seeds/Food_Pantries.csv", { headers: true, header_converters: :symbol, converters: :all}) do |row|
   Organization.create!(row.to_hash)
