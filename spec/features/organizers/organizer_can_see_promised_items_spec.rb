893a5149c33b0c06ae5b78f07bcfa5ce3bada2ae
Melissa Leigh Gore <melissaleighgore@gmail.com>
2015-07-16 21:36:58 -0400

    Organizer promised list test skeleton

---
+++ spec/features/organizers/organizer_can_see_promised_items_spec.rb
@@ -0,0 +1,20 @@
+# require "rails_helper"
+
+# feature "Organizer can see items promised to them by volunteers", %{
+#   As an organizer
+#   I want to see the items that are promised
+#   So I know what we can expect
+# } do
+#   # Acceptance Criteria
+#   # [ ] Organizer can see promised items when logged in
+#   # [ ] Promised item list changes if volunteer cancels
+
+#   scenario "Organizer can see items promised to them" do
+#     organizer = FactoryGirl.create(:organizer)
+    
+#     sign_in_as(organizer)
+
+    
+#   end
+
+# end
