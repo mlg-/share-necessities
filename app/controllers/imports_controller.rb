f439a2add3101090b29a5ccf2a4595dcb961053b
Melissa Leigh Gore <melissaleighgore@gmail.com>
2015-07-22 12:33:08 -0400

    All class methods needed for wishlist import

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -7,7 +7,6 @@ class ImportsController < ApplicationController
     @import = Import.new(import_params)
     @organization = Organization.find(params[:import][:organization_id])
     @items = Import.wishlist(@import.url)
-    binding.pry
     if @import.save
       flash[:notice] = "Your wishlist is importing."
       redirect_to import_path(@import)
